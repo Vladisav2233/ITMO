8aa66ad327031f275dc1a499a1654c803f1307fd
Vladislav <Vladislavivanovspb@gmail.com>
2022-04-15 02:20:21 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkFigure12/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] Mas = new int[5];
+Mas[0] = 10;
+Mas[1] = 11;
+Mas[2] = 12;
+Mas[3] = 13;
+Mas[4] = 4;
+
+int n = Mas.Length;
+for (int i = 0; i < n; i++)
+{
+    if (Mas[i] == i)
+        Console.WriteLine("Значение True для элемента массива " + i);
+}
+Console.WriteLine("Значение False");
+
---
+++ MyAlgorithm/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? ignition = "true";      //состояние зажигания
+double FrontLeftWheel = 0;                //значение колесного датчика давления
+int stoptime = 5000;         //время задержки опроса датчика в милисекундах
+while (ignition == "true")
+{
+    Console.WriteLine("ввести значение колесного датчика давления переднего левого колеса");
+    FrontLeftWheel = Convert.ToDouble(Console.ReadLine());
+    if (FrontLeftWheel  < 2 & FrontLeftWheel  >= 0)
+        Console.WriteLine("Проверьте левое переднее колесо " + FrontLeftWheel );
+    else if (FrontLeftWheel >=2 && FrontLeftWheel <=2.4)
+        Console.WriteLine("Левое переднее колесо " + FrontLeftWheel );
+    else if (FrontLeftWheel  > 2.4)
+        Console.WriteLine("Давление в  левом переднем колесе выше нормы " + FrontLeftWheel );
+    else
+        Console.WriteLine("Датчик неисправен");
+    Thread.Sleep(stoptime);
+    Console.WriteLine("Зажигание включено? (введиде true или false)");
+    ignition = Console.ReadLine();
+    
+}
+Console.WriteLine("Зажигание ВЫКЛ");
