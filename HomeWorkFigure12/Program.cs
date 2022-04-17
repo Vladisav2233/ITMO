50d61e25c9e544bffab188393c73e6e5fddc1bbf
Vladislav <Vladislavivanovspb@gmail.com>
2022-04-18 00:45:03 +0300

    ввел return

--- HomeWorkFigure12/Program.cs
+++ HomeWorkFigure12/Program.cs
@@ -1,16 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] Mas = new int[5];
-Mas[0] = 10;
-Mas[1] = 11;
-Mas[2] = 12;
-Mas[3] = 13;
-Mas[4] = 4;
-
-int n = Mas.Length;
-for (int i = 0; i < n; i++)
+bool go()
 {
-    if (Mas[i] == i)
-        Console.WriteLine("Значение True для элемента массива " + i);
-}
-Console.WriteLine("Значение False");
+bool result = false;
+int[] Mas = new int[5];
+    Mas[0] = 10;
+    Mas[1] = 11;
+    Mas[2] = 12;
+    Mas[3] = 13;
+    Mas[4] = 4;
 
+    int n = Mas.Length;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (Mas[i] == i)
+            Console.WriteLine("Значение True для элемента массива " + i);
+        result = true;
+    }
+    return result;
+}
+go();
